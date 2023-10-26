188a17cdfaed0ac8b0ce69f5a1f24abff942e65f
shahlairshad <shahlam144@gmail.com>
2023-10-26 14:05:20 +0530

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,9 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(){
+  print("My name is Shahla");
+  print('I am 25 years old');
+  print('I am studying in Luminar Technolab');
+  print('my mail id is shahlam144@gmail.com');
+  print('my number is 790221636');
+}
